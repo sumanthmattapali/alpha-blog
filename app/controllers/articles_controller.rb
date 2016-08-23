a90ba02e0c2dc04c321c68b409fb8c5054767517
sumanthmattapalli <sumanthmattapalli@gmail.com>
2016-08-23 19:25:56 +0000

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,58 +1,62 @@
 class ArticlesController < ApplicationController
 
- def index
-   @articles = Article.all
-  end
+before_action :set_article, only: [:edit,:update, :show, :destroy]
+def index
+@articles = Article.all
+end
 
-  def new
-    @article = Article.new
-  end
+def new
+@article = Article.new
+end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+def edit
 
-  def update
-@article = Article.find(params[:id])
-    if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
-      redirect_to article_path(@article)
-      else
-      render 'edit'
-  end
-  end
+end
 
-  def create
-    #to display the passing info at articles/new
-  #render plain: params[:article].inspect
-    #@article = Article.new(article_params)
-    #@article.save
-        #redirect_to article_show(@article)
+def update
+
+if @article.update(article_params)
+flash[:notice] = "Article was successfully updated"
+redirect_to article_path(@article)
+else
+render 'edit'
+end
+end
+
+def create
+#to display the passing info at articles/new
+#render plain: params[:article].inspect
+#@article = Article.new(article_params)
+#@article.save
+  #redirect_to article_show(@article)
 @article = Article.new(article_params)
-    if @article.save
-  flash[:notice] = "Article was sucessfully created"
-  redirect_to article_path(@article)
-      else
-  render 'new'
-    end
-  end
+if @article.save
+flash[:notice] = "Article was sucessfully created"
+redirect_to article_path(@article)
+else
+render 'new'
+end
+end
 
-  def show
+def show
 
-    @article = Article.find(params[:id])
-  end
 
-  def destroy
-  @article = Article.find(params[:id])
-    @article.destroy
-    flash[:notice] = "Article successfully deleted"
-    redirect_to articles_path
-  end
- 
-  private
-  def article_params
-  params.require(:article).permit(:title, :description)
-  end
+end
+
+def destroy
+
+@article.destroy
+flash[:notice] = "Article successfully deleted"
+redirect_to articles_path
+end
+
+private
+def set_article
+@article = Article.find(params[:id])
+end
+def article_params
+params.require(:article).permit(:title, :description)
+end
 
 
 
